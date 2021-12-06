3ecc8860fc81160bb4e595b2397c8c32395175c1
Atlas <77736272+ASproson@users.noreply.github.com>
2021-12-06 14:42:00 +0000

    Adds initial functionality for scrolling background

--- src/components/ReactGameContainer.js
+++ src/components/ReactGameContainer.js
@@ -9,10 +9,10 @@ function ReactGameContainer({
   setGameTime,
 }) {
   useEffect(() => {
-    const height = window.innerHeight;
-    const width = window.innerWidth;
+    const height = 800;
+    const width = 600;
     console.log(height);
-    document.game = new CreateGame(colorChoice, height, width);
+    document.game = new CreateGame(colorChoice, width, height);
 
     return () => {
       setGameTime(Math.round(document.game.getPlayTime() / 1000));
