75c0e283aea924c23835a24cd640740a32999640
Sachin Naranji <snaranji01@gmail.com>
2021-12-07 10:54:27 +0000

    adds initial template for HomePage

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+
+    return (
+        <main>
+            <h1>Homepage</h1>
+            <button >Log Out</button>
+        </main>
+    )
+}
+
+export default Homepage
