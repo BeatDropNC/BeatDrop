53553b3afe87271d3b2eb2a1734eb5b9e51e7bf0
Sachin Naranji <snaranji01@gmail.com>
2021-12-08 22:51:00 +0000

    updates for new 'users' document structure, and new test user

--- src/__tests__/firestore.utils.test.js
+++ src/__tests__/firestore.utils.test.js
@@ -1,10 +1,32 @@
 import { getUserByUid } from "../firebase/firestore.utils";
 
 
+/* 
+initialTestUserDocument:
+
+*uid* : {
+    authProvider: "local",
+    email: "modasi2384@hagendes.com",
+    friends: [],
+    highScores: {
+        level1: 0,
+        level2: 0,
+        level3: 0,
+        level4: 0,
+        level5: 0,
+        level6: 0,
+    },
+    scoreHistory: [],
+    uid: *uid*,
+    username: "user-signup-example", 
+}
+ */
+
+
 describe("getUserByUid", () => {
     describe("Successful requests", () => {
         test("Test 1: Returns a user object in the correct form", async () => {
-            const uid = "tSrxjGzmnqUC2ouS78h1Qfnp3833";
+            const uid = "StcKOHV1aDZsvk34HC0XpLU8ewq1";
             const userObj = await getUserByUid(uid)
             expect(userObj).toEqual(
                 expect.objectContaining({
@@ -13,26 +35,27 @@ describe("getUserByUid", () => {
                     email: expect.any(String),
                     authProvider: expect.any(String),
                     friends: expect.any(Array),
-                    scores: expect.objectContaining({
+                    highScores: expect.objectContaining({
                         level1: expect.any(Number),
                         level2: expect.any(Number),
                         level3: expect.any(Number),
                         level4: expect.any(Number),
                         level5: expect.any(Number),
                         level6: expect.any(Number),
-                    })
+                    }),
+                    scoreHistory: expect.any(Array),
                 })
             )
         })
         test("Test 2: Returns the correct user object", async () => {
-            const uid = "tSrxjGzmnqUC2ouS78h1Qfnp3833";
+            const uid = "StcKOHV1aDZsvk34HC0XpLU8ewq1";
             const userObj = await getUserByUid(uid);
             expect(userObj.username).toBe("user-signup-example");
         })
     })
     describe('Errors', () => {
         test('Test 3: Unique ID supplied not found in database', async () => {
-            const uid = "notExistentId";
+            const uid = "notAUniqueId";
             await expect(getUserByUid(uid)).rejects.toEqual({
                 status: 404,
                 msg: "404 Error: User not found"
