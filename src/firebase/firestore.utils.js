93f6081861aac65ad7c59d68a98a9b8ab605631e
Sachin Naranji <snaranji01@gmail.com>
2021-12-08 19:18:16 +0000

    updates getUserByUid to pass Test 3

--- src/firebase/firestore.utils.js
+++ src/firebase/firestore.utils.js
@@ -4,9 +4,20 @@ import { db } from "./firebase"
 
 
 const getUserByUid = async (uid) => {
-    const docRef = doc(db, "users", uid);
-    const userDoc = await getDoc(docRef);
-    return userDoc.data();
+    try {
+        const docRef = doc(db, "users", uid);
+        const userDoc = await getDoc(docRef);
+        if (userDoc.exists()) {
+            return userDoc.data();
+        } else {
+            return Promise.reject({
+                status: 404,
+                msg: "404 Error: User not found"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
