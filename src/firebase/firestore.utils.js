fac42a9b361aa63ceba3029deb437a2a23762ed1
Sachin Naranji <snaranji01@gmail.com>
2021-12-08 18:27:23 +0000

    updates to pass Test 1 for getUserByUid

--- src/firebase/firestore.utils.js
+++ src/firebase/firestore.utils.js
@@ -1,19 +1,15 @@
-import { collection, getDocs, query, where } from "firebase/firestore"
+import { getDoc, doc } from "firebase/firestore"
 import { db } from "./firebase"
 
 
+
 const getUserByUid = async (uid) => {
-    try {
-        const collectionRef = collection(db, 'users')
-        const queryToUse = query(collectionRef, where('uid', '==', uid))
-        const querySnapshot = await getDocs(queryToUse)
-        let user
-        querySnapshot.forEach((item) => {
-            user = item.data()
-        })
-        return user
-    } catch (error) {
-        console.log(error)
-        alert(error.message)
-    }
+    const docRef = doc(db, "users", uid);
+    const userDoc = await getDoc(docRef);
+    return userDoc.data();
+}
+
+
+export {
+    getUserByUid,
 }
