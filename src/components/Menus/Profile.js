27e49acbb40a0e09b43db2a3dfe9ed5f67b87994
Atlas <77736272+ASproson@users.noreply.github.com>
2021-12-10 15:56:39 +0000

    Adds profile page, avatar selection, and onclicks, and some styling

--- src/components/Menus/Profile.js
+++ src/components/Menus/Profile.js
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { UserUidContext } from '../../contexts/UserUidContext';
+import '../../styles/Profile.css'
+const data = require('../../dummy-data.json');
 
 const Profile = () => {
     const { userUid } = useContext(UserUidContext);
@@ -10,9 +12,56 @@ const Profile = () => {
             navigate('/');
         }
     }, [userUid, navigate]);
+
+    const [currentAvatar, setCurrentAvatar] = useState('https://img.favpng.com/13/21/14/sprite-animation-2d-computer-graphics-game-character-png-favpng-JfchZaT8PcD0SyBxicgteE54g.jpg');
+
+
+
+    // Temp character sprites
+    const spriteLinks = [
+        'https://img.favpng.com/13/21/14/sprite-animation-2d-computer-graphics-game-character-png-favpng-JfchZaT8PcD0SyBxicgteE54g.jpg',
+        'https://img.favpng.com/14/1/21/cartoon-character-animation-game-png-favpng-dt9BZDhZxuwemLxuqz1pmXPXy.jpg',
+        'https://www.pinclipart.com/picdir/middle/559-5591551_2d-character-sprite-transparent-clipart.png',
+        'https://www.pinclipart.com/picdir/middle/559-5599557_rpg-character-png-2d-clipart.png',
+        'https://www.pinclipart.com/picdir/middle/495-4951686_10-nobodys-hero-2d-sprites-character-egypt-clipart.png',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKWnct6W7vy_S_nYF3lKgXqtvBBrX2Dw3wz7naEZi8xHpw6zfxXD2NY1VfTYvS-hAFbpU&usqp=CAU',
+    ]
+
+    const badgesLink = [
+        'https://cdn.imgbin.com/16/21/8/imgbin-gold-badge-white-seal-badge-with-gold-decor-round-gold-frame-illutsration-7uCZV8fzRdEett5xPSEsXdBdw.jpg',
+        'https://toppng.com/uploads/preview/seal-badge-blue-png-11546988382eu7lmbuich.png',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR83MNqrxjXDb0yOdURuaOWFjFFGzRBjUTP6Lxe_i7q-Z-6pcwXruAhNY_V7tJn5GPUGf8&usqp=CAU',
+    ]
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className='Profile'>
+            <h1>{data.users.userDocument1.username}</h1>
+            <h2>Avatar</h2>
+            <div className='current_avatar'></div>
+            <img src={currentAvatar}></img>
+            <h2 className='character_select_title'>Character Select</h2>
+            <div className='character_select_container'>
+                <div className='character_select'>
+                    {spriteLinks.map((sprite) => {
+                        return (
+                            <button key={sprite} onClick={() => {
+                                setCurrentAvatar(sprite);
+                                // Patch user with new sprite
+                            }}>
+                                <img className={sprite} key={sprite} src={sprite}></img>
+                            </button>
+                        )
+                    })}
+                </div>
+            </div>
+            <h2>Badges</h2>
+            <div className='user_badges'>
+                {badgesLink.map((badge) => {
+                    return (
+                        <img className={badge} key={badge} src={badge}></img>
+                    )
+                })}
+            </div>
             <Link to='/main-menu'>Return to Main Menu</Link>
         </div>
     )
