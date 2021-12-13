09e509f1ae068f65d22ebf91680fb4a2fc7431eb
Sachin Naranji <snaranji01@gmail.com>
2021-12-13 23:28:55 +0000

    updates to now fetch rendered data from firestore

--- src/components/Menus/GlobalLeaderboard.jsx
+++ src/components/Menus/GlobalLeaderboard.jsx
@@ -1,15 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserUidContext } from '../../contexts/UserUidContext';
+import { getGlobalLeaderboard } from '../../firebase/firebase.api';
 import '../../styles/GlobalLeaderboard.css'
-const { globalLeaderboard: dummyData } = require('../../dummy-data.json');
 
 const GlobalLeaderboard = () => {
 
-    const [levelToShow, setLevelToShow] = useState(0)
+    const [levelToShow, setLevelToShow] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [globalLeaderboardData, setGlobalLeaderboardData] = useState(undefined);
 
-    const { userUid } = useContext(UserUidContext)
-    const navigate = useNavigate()
+    const { userUid } = useContext(UserUidContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!userUid) {
@@ -17,54 +19,80 @@ const GlobalLeaderboard = () => {
         }
     }, [userUid, navigate])
 
-    const sortedData = dummyData.globalLeaderboardDocument[`level${levelToShow + 1}`].sort(function(a, b) {
-        return b.score - a.score
-    })
+    useEffect(() => {
+        let isMounted = true;
 
-    return (
-        <main className='globalLeaderboard'>
-            <h1> Global Leaderboard </h1>
-            <div className="leaderboard-chooser">
-                <Link to='/leaderboards/personal'><button>Personal</button></Link>
-                <Link to='/leaderboards/global'><button>Global</button></Link>
-            </div>
-            <div>
-                <h2>Level {levelToShow + 1}</h2>
-                <div className='levelButtons'>
-                {Object.keys(dummyData.globalLeaderboardDocument).map((level, index) => {
-                    return (
-                        <button
-                            key={'buttons' + level + index}
-                            onClick={() => {
-                                setLevelToShow(index)
-                            }}
-                        >
-                            Level {index + 1}
-                        </button>
-                    )
-                })}
-            </div>
-            <div className="leaderboard-div">       
-                    {sortedData.map((data)=>{
-                        return (
-                            <div key={data.score}>
-                            <p>
-                                {data.score}
-                            </p>
-                            <p>
-                                {data.user}
-                            </p>
-                            </div>
-                        )
-                    })}
-            </div>
-            </div>
-            <div className='main_menu'>
-                <Link to='/main-menu'>Return to Main Menu</Link>
-            </div>
-            
-        </main>
-    );
+        const fetchGlobalLeaderboardData = async () => {
+            let isMounted = true;
+            const response = await getGlobalLeaderboard();
+            if (isMounted) {
+                setGlobalLeaderboardData(response)
+                setLoading(false);
+            }
+        }
+        fetchGlobalLeaderboardData();
+
+        return () => {
+            isMounted = false;
+        }
+    }, []);
+
+    console.log(globalLeaderboardData)
+
+    if (loading) {
+        return <p>Loading...</p>
+    } else {
+        const sortedData = globalLeaderboardData[`level${levelToShow + 1}`]["scoresList"].sort(function (a, b) {
+            return b.score - a.score
+        })
+        console.log(sortedData)
+        return (
+            <main className='globalLeaderboard'>
+                <h1> Global Leaderboard </h1>
+                <div className="leaderboard-chooser">
+                    <Link to='/personal-leaderboard'><button>Personal</button></Link>
+                    <Link to='/global-leaderboard'><button>Global</button></Link>
+                </div>
+                <div>
+                    <h2>Level {levelToShow + 1}</h2>
+                    <div className='levelButtons'>
+                        {Object.keys(globalLeaderboardData).map((level, index) => {
+                            return (
+                                <button
+                                    key={'buttons' + level + index}
+                                    onClick={() => {
+                                        setLevelToShow(index)
+                                    }}
+                                >
+                                    Level {index + 1}
+                                </button>
+                            )
+                        })}
+                    </div>
+                    <div className="leaderboard-div">
+                        {sortedData.map((data) => {
+                            return (
+                                <div key={data.score}>
+                                    <p>
+                                        {data.score}
+                                    </p>
+                                    <p>
+                                        {data.user}
+                                    </p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className='main_menu'>
+                    <Link to='/main-menu'>Return to Main Menu</Link>
+                </div>
+            </main>
+        );
+    }
+
+
+
 };
 
 export default GlobalLeaderboard;
