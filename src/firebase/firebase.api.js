617957638f692845d6846eaffba08b801d0f7306
Sachin Naranji <snaranji01@gmail.com>
2021-12-13 17:33:08 +0000

    adds getUserDoc api function

---
+++ src/firebase/firebase.api.js
@@ -0,0 +1,23 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from './firebase';
+
+const getUserDoc = async (uid) => {
+    if(uid === undefined) {
+        console.log("User Unique ID is undefined")
+    }
+    const userDocRef = doc(db, "users", uid);
+    try {
+        const userDoc = await getDoc(userDocRef)
+        if(userDoc.exists()) {
+            return userDoc.data()
+        } else {
+            console.log("User document not found")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    getUserDoc,
+}
