d70ab055f751a6be56f3e8c3ba2fda8530a9ee1d
Sachin Naranji <snaranji01@gmail.com>
2021-12-13 18:07:46 +0000

    adds user information to context

--- src/firebase/firebase.api.js
+++ src/firebase/firebase.api.js
@@ -2,19 +2,20 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
 const getUserDoc = async (uid) => {
-    if(uid === undefined) {
+    if (uid === undefined) {
         console.log("User Unique ID is undefined")
-    }
-    const userDocRef = doc(db, "users", uid);
-    try {
-        const userDoc = await getDoc(userDocRef)
-        if(userDoc.exists()) {
-            return userDoc.data()
-        } else {
-            console.log("User document not found")
+    } else {
+        const userDocRef = doc(db, "users", uid);
+        try {
+            const userDoc = await getDoc(userDocRef)
+            if (userDoc.exists()) {
+                return userDoc.data()
+            } else {
+                console.log("User document not found")
+            }
+        } catch (error) {
+            console.log(error)
         }
-    } catch (error) {
-        console.log(error)
     }
 }
 
