90241dfc12d41abc94c6c90e325748680afa08c2
Sachin Naranji <snaranji01@gmail.com>
2021-12-13 21:20:01 +0000

    updates to post new user  highscore to firestore 'users' collection

--- src/components/ReactGameContainer.js
+++ src/components/ReactGameContainer.js
@@ -1,16 +1,42 @@
+import { Timestamp } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { UserUidContext } from "../contexts/UserUidContext";
+import { patchUserScores } from "../firebase/firebase.api";
 import CreateGame from "../phaser/CreateGame";
 import "../styles/ReactGameContainer.css";
+import { currentDateTimeString } from "../utils";
 function ReactGameContainer({
   levelChoice,
   setGameTime,
 }) {
+  const { userInformation, setUserInformation, userUid } = useContext(UserUidContext);
   const [gameEnd, setGameEnd] = useState(false);
+  const submitScore = async (newScore) => {
+    const currentLevelScores = userInformation["userScores"][levelChoice];
 
-  const submitScore = async (score) => {
-    console.log(score)
+    currentLevelScores.sort((a,b) => b.score - a.score);
+    if (newScore >= currentLevelScores[4].score) {
+      const newLevelScores = [...currentLevelScores];
+      newLevelScores.push({ score: newScore, timeCompletedAt: currentDateTimeString() })
+      newLevelScores.sort((a, b) => b.score - a.score);
+      newLevelScores.pop();
+
+      // set local user state
+      setUserInformation(currentUserInformation => {
+        const newUserInformation = JSON.parse(JSON.stringify(currentUserInformation));
+        newUserInformation["userScores"][levelChoice] = newLevelScores;
+        return newUserInformation
+      })
+
+      // post new score to 'users' collection on firestore
+      patchUserScores(userUid, newLevelScores, levelChoice)
+      .then(() => {
+        console.log("New high score was updated!")
+      })      
+    } else {
+      console.log("Not a high score.")
+    }
   }
 
   const navigate = useNavigate();
