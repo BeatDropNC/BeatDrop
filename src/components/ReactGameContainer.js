4f12116d3c860ba0cddc183c4e75a70d984d03c0
Sachin Naranji <snaranji01@gmail.com>
2021-12-14 13:36:41 +0000

    adds base post request to create new activity when a badge is earned

--- src/components/ReactGameContainer.js
+++ src/components/ReactGameContainer.js
@@ -1,8 +1,7 @@
-import { Timestamp } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { UserUidContext } from "../contexts/UserUidContext";
-import { getGlobalLeaderboard, patchGlobalLeaderboardScore, patchUserScores } from "../firebase/firebase.api";
+import { getGlobalLeaderboard, patchGlobalLeaderboardScore, patchUserScores, postNewActivity } from "../firebase/firebase.api";
 import CreateGame from "../phaser/CreateGame";
 import "../styles/ReactGameContainer.css";
 import { currentDateTimeString } from "../utils";
@@ -16,6 +15,7 @@ function ReactGameContainer({
     const currentLevelScores = userInformation["userScores"][levelChoice];
 
     currentLevelScores.sort((a, b) => b.score - a.score);
+    // 
     if (newScore >= currentLevelScores[currentLevelScores.length - 1].score) {
       const newLevelScores = [...currentLevelScores];
       newLevelScores.push({ score: newScore, timeCompletedAt: currentDateTimeString() })
@@ -39,7 +39,6 @@ function ReactGameContainer({
       const response = await getGlobalLeaderboard();
       const currentGlobalLevelScores = response[levelChoice]["scoresList"];
       currentGlobalLevelScores.sort((a, b) => b.score - a.score);
-      console.log(currentGlobalLevelScores)
       if (newScore >= currentGlobalLevelScores[currentGlobalLevelScores.length-1].score) {
         const newGlobalLevelScores = [...currentGlobalLevelScores];
         newGlobalLevelScores.push({ score: newScore, timeCompletedAt: currentDateTimeString() })
@@ -58,6 +57,21 @@ function ReactGameContainer({
     } else {
       console.log("Not a high score.")
     }
+
+    // send any new badges
+    let badge;
+    if(newScore > 0) {
+      badge = "badge1";
+    }
+
+    if(badge !== undefined) {
+      // console.log(userInformation.username, "Username")
+      // console.log(badge, "badge")
+      // console.log(newScore, "newScore")
+      // console.log(levelChoice, "level choice")
+      await postNewActivity(userInformation.username, badge, newScore, levelChoice)
+      .then(() => console.log("posted new achievement activity"))
+    }
   }
 
   const navigate = useNavigate();
@@ -69,7 +83,6 @@ function ReactGameContainer({
   useEffect(() => {
     const height = 800;
     const width = 600;
-    console.log(height);
     document.game = new CreateGame(levelChoice, width, height, destroyPhaserGame, submitScore);
 
     return () => {
