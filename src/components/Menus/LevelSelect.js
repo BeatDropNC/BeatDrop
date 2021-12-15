dd5d96ac5e1114b3a77564507383e50201f7ba7c
marsj96 <jack.oliver.marsden@gmail.com>
2021-12-15 12:37:24 +0000

    adds level select images and functionality

--- src/components/Menus/LevelSelect.js
+++ src/components/Menus/LevelSelect.js
@@ -6,6 +6,13 @@ import arrowRight from '../../assets/next.png'
 import arrowLeft from '../../assets/next.png'
 import play from '../../assets/buttons/Play.png';
 import returnButton from '../../assets/buttons/Return.png';
+import level1 from '../../assets/levelSelect/level1.png'
+import level2 from '../../assets/levelSelect/level2.png'
+import level3 from '../../assets/levelSelect/level3.png'
+import level4 from '../../assets/levelSelect/level4.png'
+import level5 from '../../assets/levelSelect/level5.png'
+import { useState } from 'react/cjs/react.development';
+
 
 function LevelSelect({ setLevelChoice }) {
 
@@ -17,6 +24,18 @@ function LevelSelect({ setLevelChoice }) {
     }
   }, [userUid, navigate]);
 
+  
+
+  const levelInfo = [
+    {levelName: "level1", img: level1}, 
+    {levelName: "level2", img: level2}, 
+    {levelName: "level3", img: level3}, 
+    {levelName: "level4", img: level4}, 
+    {levelName: "level5", img: level5}, 
+  ]
+
+  const [currentLevel, setCurrentLevel] = useState(0)
+
   // 01: ruaHERO  		  === lets_go_60_sec
   // 02: kazaCHOK		    === partying_in_russia_60_sec
   // 03: gggRIME		    === bass_trap_60_sec
@@ -26,22 +45,40 @@ function LevelSelect({ setLevelChoice }) {
   return (
     <div className="Menu">
       <h1 className='title'>Level<br></br>Selector</h1>
-      <h2>Level title on change</h2>
+      <h2>Level {currentLevel+1}</h2>
+
+      {/* Only renders right button if level select is not 0 */}
       <div className='level_selector'>
-      <img className='arrowLeft' src={arrowLeft}/>
-        <img className='bgimage'/>
-        <img className='arrowRight' src={arrowRight}/>
+        {currentLevel === 0 ? 
+        ""
+         : 
+         <img className='arrowLeft' src={arrowLeft} onClick={()=>{
+          setCurrentLevel((prevValue)=>{
+            let newLevel = prevValue
+            newLevel--
+            return newLevel
+          })
+        }}/>
+        }
+      
+        <img className='bgimage' src={levelInfo[currentLevel].img}/>
+
+        {/* Only renders left button if level select is not 5 */}
+        {currentLevel === 4 ? "" :
+        <img className='arrowRight' src={arrowRight} onClick={()=>{
+          setCurrentLevel((prevValue)=>{
+            let newLevel = prevValue
+            newLevel++
+            return newLevel
+          })
+        }}/>
+        }
+        
       </div>
-      <div className="button-container">
-        <div className='buttons'>
-          <button className="red button" onClick={() => {setLevelChoice('level1')}}>Level 1</button>
-          <button className="red button" onClick={() => {setLevelChoice('level2')}}>Level 2</button>
-          <button className="red button" onClick={() => {setLevelChoice('level3')}}>Level 3</button>
-          <button className="red button" onClick={() => {setLevelChoice('level4')}}>Level 4</button>
-          <button className="red button" onClick={() => {setLevelChoice('level5')}}>Level 5</button>
-        </div> 
-      </div>
-      <Link id={'start-button'} className={"play-button"} to={"/newgame"}><img className='pixel-buttons-play' src={play}/></Link>
+      <Link id={'start-button'} className={"play-button"} to={"/newgame"}><img className='pixel-buttons-play' src={play} 
+      onClick={() => {setLevelChoice(`level${currentLevel+1}`)}}
+      />
+      </Link>
       <Link id={'main_menu'} className={'main_menu'} to='/main-menu'><img className='pixel-buttons-return' src={returnButton}/></Link>
     </div>
   );
